c414fe8f16a64511d73ae74d95761b1775566c0b
wangshumin <wang.shumin@bluebank.com.cn>
2019-03-26 17:53:13 +0800

    add remote code,layer-list drawable,transition drawable code

--- app/src/main/java/tech/xiaosuo/com/remoteviewsimple/MainActivity.java
+++ app/src/main/java/tech/xiaosuo/com/remoteviewsimple/MainActivity.java
@@ -1,7 +1,15 @@
 package tech.xiaosuo.com.remoteviewsimple;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setTicker("ticker title");
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setWhen(System.currentTimeMillis());
+        builder.setAutoCancel(true);
+        Intent intent = new Intent(this,TestActivity1.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentTitle("Contenttitle");
+        builder.setContentText("contentText");
+        builder.setContentIntent(pendingIntent);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.remote_notification);
+        remoteViews.setTextViewText(R.id.remote_text_view,"remoteText");
+        remoteViews.setImageViewResource(R.id.remote_img_view,R.drawable.ic_launcher_foreground);
+        remoteViews.setOnClickPendingIntent(R.id.remote_button_view,pendingIntent);
+        builder.setContent(remoteViews);
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,notification);
+
+        TextView textView = (TextView)findViewById(R.id.transition_textview);
+        TransitionDrawable drawable  = (TransitionDrawable)textView.getBackground();
+        drawable.startTransition(5000);
     }
+
+
+
 }
---
+++ app/src/main/java/tech/xiaosuo/com/remoteviewsimple/TestActivity1.java
@@ -0,0 +1,13 @@
+package tech.xiaosuo.com.remoteviewsimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+    }
+}
